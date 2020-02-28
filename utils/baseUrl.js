95222481d6ab6a11923f48737d2d1d42047ae362
muhannadjudah <muhannadjudah@gmail.com>
2020-02-28 23:02:32 +0100

    fix url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://e-commerce-apps.now.sh'
+    ? 'https://e-commerce-app.now.sh'
     : 'http://localhost:3000';
 
 export default baseUrl;
